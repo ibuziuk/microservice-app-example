d1384d25c856e5f7ae5af768f89192a4db1e927b
Ilya Buziuk <ibuziuk@redhat.com>
2018-05-08 19:45:19 +0000

    Fixing bug
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
+++ users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
@@ -1,16 +1,41 @@
 package com.elgris.usersapi;
 
+import io.jsonwebtoken.Claims;
+
+import javax.servlet.http.HttpServletRequest;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.elgris.usersapi.api.UsersController;
+import com.elgris.usersapi.models.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsersApiApplicationTests {
+    
+    @Autowired
+    UsersController controller;
 
-	@Test
-	public void contextLoads() {
+	@SuppressWarnings("deprecation")
+    @Test
+	public void getUser() {
+	    String username = "admin";
+	    String firstName = "Foo";
+	    HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+	    Claims claims = Mockito.mock(Claims.class);
+	    
+	    Mockito.when(request.getAttribute("claims")).thenReturn(claims);
+	    Mockito.when(claims.get("username")).thenReturn(username);
+	    
+	    User user = controller.getUser(request, username);
+	    Assert.assertEquals(firstName, user.getFirstname());
 	}
 
 }
